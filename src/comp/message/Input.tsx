2cda342d98bd9c7807ecc2f31e4de11b673fd90f
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-06-18 20:21:08 -0400

    all changes before making backEnd

--- src/comp/message/Input.tsx
+++ src/comp/message/Input.tsx
@@ -1,16 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function Input() {
+  const [user, setUser] = useState('');
+  const [message, setMessage] = useState('');
 
-  const postData = () => {
-    console.log('hello')
-  }
+  const  postData = async () => {
+    console.log(`Hello ${user} message is: ${message}`);
+    setMessage('')
+    setUser('')
+
+    const data = {
+      user,
+      message
+    }
+
+    console.log(data)
+
+    // try {
+    //   const resp = await axios.post(
+    //     '',
+    //     // 'http://localhost:4000/create-checkout-session', POST, GET, DELETE, PUT
+    //     {
+    //       data,
+    //     }
+    //   );
+    //   console.log(resp);
+
+    //   // const { url_ } = resp.data;
+   
+    // } catch (error) {
+    //   console.log(error);
+    // }
+   
+
+  };
 
   return (
     <div>
-        <input placeholder='user' type="text" />
-        <input placeholder='message' type="text" />
-        <button onClick={() => postData()}>Send Me</button>
+      <input
+        onChange={(e) => {
+          setUser(e.target.value);
+        }}
+        placeholder="user"
+        value={user}
+        type="text"
+      />
+      <input
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }}
+        placeholder="message"
+        value={message}
+        type="text"
+      />
+      <button onClick={() => postData()}>Send Me</button>
     </div>
-  )
+  );
 }
