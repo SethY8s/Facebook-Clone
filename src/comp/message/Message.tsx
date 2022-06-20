9c69abaa17118ed2481aacb714f27629c27f11be
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-06-20 16:30:03 -0400

    broken code, but figuring out stroage of friends and messages

--- src/comp/message/Message.tsx
+++ src/comp/message/Message.tsx
@@ -3,6 +3,19 @@ import React, { useState } from 'react';
 export default function Message() {
   const [message, setMessage] = useState('j');
   const [messages, setMessages] = useState(['hello', 'there']);
+  const [userData, setUserData] = useState(
+    {
+      friends: { 
+        sarah : ["hey", "what's up?"],
+        blake: ["can I have your homework?"]
+      },
+  }
+  )
+
+  for(let friend:Array in userData.friends){
+    
+    console.log(userData.friends[friend])
+  }
 
   return (
     <div>
@@ -10,6 +23,9 @@ export default function Message() {
       {messages.map((el) => (
         <p key={el}>{el}</p>
       ))}
+
+       
+
     </div>
   );
 }
