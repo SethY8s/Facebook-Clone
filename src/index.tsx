aa3ce84f3d8138e1e1ae32be5b436a72210076ac
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-06-22 16:24:14 -0400

    added redux for state management

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './comp/App';
 
+import { Provider } from 'react-redux'
+import store from './redux/store';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
