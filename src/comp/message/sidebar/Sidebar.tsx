cafb65f7d8dd02977c5e553dc7b7d74bfdb44245
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-07-19 21:13:55 -0400

    basic styling to sidebar

--- src/comp/message/sidebar/Sidebar.tsx
+++ src/comp/message/sidebar/Sidebar.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
-import './_sidebar.scss'
+import React from 'react';
+import './_sidebar.scss';
+const photo = require('../../../images/photo.jpg');
 
 export default function Sidebar() {
   return (
-    <div id='sidebar'>Sidebar</div>
-  )
+    <div id="sidebar">
+      <div className='d-flex flex-column'>
+        <div className="d-flex">
+          <img className="w-25" src={photo} alt="friend" />
+          <span>
+            <h6>Marco</h6>
+            <p>Hey Bro</p>
+          </span>
+        </div>
+        <div className="d-flex">
+          <img className="w-25" src={photo} alt="friend" />
+          <span>
+            <h6>Prem</h6>
+            <p>Love you</p>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
