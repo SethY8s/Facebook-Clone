27854a3aa67f34456a4aa5406999f2bd077b6ef8
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-07-29 21:08:01 -0400

    formatting

--- src/redux/users/user-actions.js
+++ src/redux/users/user-actions.js
@@ -1,13 +1,14 @@
 import * as actionTypes from './user-types';
 
-// export const sendMessage = (itemMessage) => {
-//   return {
-//     types: actionTypes.ADD_MESSAGE,
-//     payload: {
-//       id: itemMessage,
-//     },
-//   };
-// };
+export const sendMessage = (newMessage, currentFriend) => {
+  return {
+    types: actionTypes.ADD_MESSAGE,
+    payload: {
+      id: newMessage,
+      friend: currentFriend,
+    },
+  };
+};
 
 // export const addFriend = (friend) => {
 //   return {
--- src/redux/users/user-reducer.js
+++ src/redux/users/user-reducer.js
@@ -53,8 +53,10 @@ const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     // case actionTypes.ADD_FRIEND:
     //   return {};
-    // case actionTypes.ADD_MESSAGE:
-    //   return {};
+    case actionTypes.ADD_MESSAGE:
+      return {
+        ...state,
+      };
     // case actionTypes.DELETE_FRIEND:
     //   return {};
     case actionTypes.LOAD_USER:
